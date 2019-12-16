f84552d670b75b04da3c41a90adaff9fcb45a994
Urs Albisser <u.albisser@gmail.com>
2019-12-16 14:22:02 +0100

    Spring Boot kick-start for the Game Console App

---
+++ src/main/java/ch/zhaw/gameconsoleapp/GameConsoleAppApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.gameconsoleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameConsoleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameConsoleAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/zhaw/gameconsoleapp/GameConsoleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.gameconsoleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameConsoleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
