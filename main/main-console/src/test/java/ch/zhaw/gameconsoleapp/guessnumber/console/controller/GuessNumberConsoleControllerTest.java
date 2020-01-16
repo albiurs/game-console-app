ebf86a03e58f2867df8a1e981d96c12f56dea4af
Urs Albisser <u.albisser@gmail.com>
2020-01-16 20:11:11 +0100

    update guessnumber play range

---
+++ main/main-console/src/test/java/ch/zhaw/gameconsoleapp/guessnumber/console/controller/GuessNumberConsoleControllerTest.java
@@ -0,0 +1,28 @@
+package ch.zhaw.gameconsoleapp.guessnumber.console.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * GuessNumberConsoleControllerTest
+ *
+ * @author created by Urs Albisser, on 2020-01-16
+ * @version 0.1
+ */
+class GuessNumberConsoleControllerTest {
+
+	@BeforeEach
+	void setUp() {
+	}
+
+	@AfterEach
+	void tearDown() {
+	}
+
+	@Test
+	void startGame() {
+	}
+}
