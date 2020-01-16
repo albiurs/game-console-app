472f97b71eed909c6ba264f5525db60059c32af5
Kiarodan <daniankiarostami@gmail.com>
2020-01-16 21:16:29 +0100

    Testclass

---
+++ games/tictactoe-console/src/Test/ch/zhaw/gameconsoleapp/tictactoe/TicTacToeGameTest.java
@@ -0,0 +1,14 @@
+package ch.zhaw.gameconsoleapp.tictactoe;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TicTacToeGameTest {
+
+    @Test
+
+    public void Test(){
+        TicTacToeGame test = new TicTacToeGame();
+    }
+}
